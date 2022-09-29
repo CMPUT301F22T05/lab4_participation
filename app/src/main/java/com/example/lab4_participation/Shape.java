949c6a73871bbdce6bb469c57e56b7b3d58f0d53
Jianxi Wang <wangjianxi@wangjianxideMacBook-Pro.local>
2022-09-28 20:39:22 -0600

    fix string syntax

--- app/src/main/java/com/example/lab4_participation/Shape.java
+++ app/src/main/java/com/example/lab4_participation/Shape.java
@@ -3,5 +3,5 @@ package com.example.lab4_participation;
 public abstract class Shape {
     private int x;
     private int y;
-    public string color = "violet";
+    public String color = "violet";
 }
