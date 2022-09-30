2144eea9c84741146725d716d39b76d5e70cbb24
JadeMJ <moorejac@ualberta.ca>
2022-09-29 18:46:21 -0600

    Update Shape.java
    Changed color to Purple - Jade

--- app/src/main/java/com/example/lab4_participation/Shape.java
+++ app/src/main/java/com/example/lab4_participation/Shape.java
@@ -3,5 +3,5 @@ package com.example.lab4_participation;
 public abstract class Shape {
     private int x;
     private int y;
-    public String color = "Black";
+    public String color = "Purple";
 }
